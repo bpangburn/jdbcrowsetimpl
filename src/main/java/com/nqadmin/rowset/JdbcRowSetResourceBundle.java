89bd976452ddad5cf4c42f8ba1c20a003256a53f
Ernie Rael <errael@raelity.com>
2020-09-08 16:05:06 +0100

    Use swingset package name for ResourceBundle

--- src/main/java/com/nqadmin/rowset/JdbcRowSetResourceBundle.java
+++ src/main/java/com/nqadmin/rowset/JdbcRowSetResourceBundle.java
@@ -84,7 +84,7 @@ public class JdbcRowSetResourceBundle implements Serializable {
      * The variable where the default resource bundle will
      * be placed.
      **/
-    private static final String PATH = "com/sun/rowset/RowSetResourceBundle";
+    private static final String PATH = "com/nqadmin/rowset/RowSetResourceBundle";
 
     /**
      * The constructor which initializes the resource bundle.
